50f7ad4ed6b96be47e10f4ba84bc71dd4eb2d333
DBsee1 <117823925+DBsee1@users.noreply.github.com>
2023-11-12 11:31:58 +0100

    Update command_handler.c

--- command_handler.c
+++ command_handler.c
@@ -1,6 +1,6 @@
 #include "alx.h"
 /**
- * execute_proc - similar to puts in C
+ * execute_proc - Custom function to output a string similar to puts in C
  * @cmd: a pointer the integer we want to set to 402
  *
  * Return: int
@@ -8,17 +8,17 @@
 void execute_proc(char **cmd)
 {
 
-	char *parametro = (*(cmd + 1));
+	char *parameter = (*(cmd + 1));
 	char *s, *slash = "/";
 	char *o;
 
-	char *vartoprint = *cmd;
+	char *print_cmd = *cmd;
 	char *argv[4];
 
 	if ((access(cmd[0], F_OK) == 0))
 	{
 		argv[0] = cmd[0];
-		argv[1] = parametro;
+		argv[1] = parameter;
 		argv[2] = ".";
 		argv[3] = NULL;
 
@@ -29,14 +29,14 @@ void execute_proc(char **cmd)
 	}
 	else
 	{
-		o = find_command(vartoprint);
+		o = find_command(print_cmd);
 
 		slash = str_concat(o, slash);
 
 		s = str_concat(slash, *cmd);
 
 		argv[0] = s;
-		argv[1] = parametro;
+		argv[1] = parameter;
 		argv[2] = ".";
 		argv[3] = NULL;
 
